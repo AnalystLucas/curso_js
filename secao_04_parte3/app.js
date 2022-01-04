7d1effd078a40d84825957d2ffb5607bf6d78879
Lucas Moreira <lucasmoreiira.v@hotmail.com>
2022-01-04 09:40:53 -0300

    Trabalhando com If/Else Operado 'E'

---
+++ secao_04_parte3/app.js
@@ -0,0 +1,20 @@
+/*
+IF / ELSE JS
+*/
+
+let nome = "Lucas";
+let sobreNome = "Moreira" ;
+let idade = 26;
+const numCliente = 095864;
+
+let valorEmprestimo = 150000;
+let taxaDeJuros = 10;
+let ehBomPagador = "SIM"; // tipo de variavel string
+
+ehBomPagador = true; //trocando a Variavel para boolean
+
+if (ehBomPagador){
+    console.log("Cliente: " + nome + " é bom pagador !");
+}else{
+    console.log("Cliente: " + nome + " não é bom pagador !!");
+}
