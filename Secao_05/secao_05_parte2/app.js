cb6ea00d73c9b20b945f9d4d85a452d48f90c37a
Lucas Moreira <lucasmoreiira.v@hotmail.com>
2022-01-23 19:36:52 -0300

    Laço FOR, incrementando e decrementado

---
+++ Secao_05/secao_05_parte2/app.js
@@ -0,0 +1,12 @@
+//Contando até 50 !
+for (let i = 1; i <= 50; i++){
+    console.log(i);
+}
+
+//Contagem Regresiva
+for (let r = 5; r >= 0; r--) {
+    console.log(r);
+    if(r == 0)  {
+        console.log("Happy New Year !!! Welcome 2022");
+    }
+}
