c42dc3f65c2ee19c1dec5e82df0b77c1b3bf6ce4
Lucas Moreira <lucasmoreiira.v@hotmail.com>
2022-01-24 02:51:15 -0300

    Desafio 2 Media de Temperatura com WHILE

--- Secao_05/secao_05_parte7_desafio2/app.js
+++ Secao_05/secao_05_parte7_desafio2/app.js
@@ -1,7 +1,18 @@
-console.log("LOJA QUASE DOIS - TABELA DE PREÇO");
+console.log("Meteorologia");
 
-for (let cont = 1; cont <= 50; cont++) {
-    console.log(`${cont} - R$ ${cont * 1.99}`);
-    // console.log(cont + " - R$: " + cont * 1.99);
+let temp;
+let soma = 0;
+let media = 0;
+let cont = 0;
 
+while(true){
+    temp = parseInt(prompt("Digita o valor de Temperatura"));
+    soma += temp; //Somando as Temperaturas.
+
+    if(temp == 0){
+        media = soma; //Recebendo as Somatorias de Temperaturas
+        console.log(media / cont); // Calculando a Media e Exibindo.
+        break; //Saindo do Laço.
+    }
+    cont++;
 }
