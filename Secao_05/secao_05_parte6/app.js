4d666119164faf16b6def68be813e2be89e79ff0
Lucas Moreira <lucasmoreiira.v@hotmail.com>
2022-01-24 02:14:21 -0300

    Usando Continue no Laço FOR

--- Secao_05/secao_05_parte6/app.js
+++ Secao_05/secao_05_parte6/app.js
@@ -1,33 +1,6 @@
-let valorEmprestimo;
-let idade = 24;
-let taxaDeJuros;
-let numAnos;
-let juros;
-let montante;
-
-if(idade >= 18 && idade <=25){
-    taxaDeJuros = 0.09;
-}else if(idade >= 26 && idade <=35){
-    taxaDeJuros = 0.08;
-}else if(idade >= 36 && idade <=50){
-    taxaDeJuros = 0.07;
-}else{
-    taxaDeJuros = 0.06;
+for (let cont = 1; cont <= 20; cont++) {
+    if(cont % 2 == 1){
+        continue;
+    };
+    console.log(cont);
 }
-
-// juros = valorEmprestimo * taxaDeJuros * numAnos;
-// montante = juros + valorEmprestimo;
-
-
-    
-while (true){
-    valorEmprestimo = parseInt(prompt("Valor do Emprestimo"));
-    if(valorEmprestimo === 0){
-        break;
-    }
-    numAnos = parseInt(prompt("Digite em quantos Anos de Duração do Emprestimo"));
-    juros = valorEmprestimo * taxaDeJuros * numAnos;
-    montante = juros + valorEmprestimo;
-    console.log(montante);
-}
-
