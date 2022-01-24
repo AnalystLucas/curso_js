05a5cd9feb83211d47fc7a4752800bfdaee14cc5
Lucas Moreira <lucasmoreiira.v@hotmail.com>
2022-01-24 20:20:22 -0300

    Preparando Seção 06 Escopo de Função

---
+++ Secao_06/secao_06_parte5/app.js
@@ -0,0 +1,30 @@
+//Expressão de Função.
+// const media = function calculaMedia(n1, n2, n3){
+//     let mediaNum = (n1 + n2 + n3 ) / 3;
+//     return mediaNum;
+// }
+// console.log(media(9,7,8));
+
+//Expressão de Funções Anonima
+// const media = function (n1, n2, n3){
+//     let mediaNum = (n1 + n2 + n3 ) / 3;
+//     return mediaNum;
+// }
+// console.log(media(9,7,8));
+
+//Arrow Function
+// const media = (n1, n2 ,n3) =>{
+//     let mediaNum = (n1 + n2 + n3) / 3;
+//     return mediaNum;
+// }
+// console.log(media(9,7,8));
+
+//Arrow Function sem chaves simplificando
+// const areaQuadrado = lado => {
+//     return lado * lado;
+// }
+// console.log(areaQuadrado(10));
+
+//Arrow Function mais simplificada dependendo do cenário sem return e chaves "{}"
+const areaQuadrado = lado => lado * lado;
+console.log(areaQuadrado(10));
