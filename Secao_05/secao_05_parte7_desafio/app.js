a5d4b50c673adc60788144648432627214cfd3b4
Lucas Moreira <lucasmoreiira.v@hotmail.com>
2022-01-24 02:31:01 -0300

    Desafio Tabela de Preço com FOR

--- Secao_05/secao_05_parte7_desafio/app.js
+++ Secao_05/secao_05_parte7_desafio/app.js
@@ -1,6 +1,7 @@
-for (let cont = 1; cont <= 20; cont++) {
-    if(cont % 2 == 1){
-        continue;
-    };
-    console.log(cont);
+console.log("LOJA QUASE DOIS - TABELA DE PREÇO");
+
+for (let cont = 1; cont <= 50; cont++) {
+    // console.log(`${cont} - R$ ${cont * 1.99}`);
+    console.log(cont + " - R$: " + cont * 1.99);
+
 }
