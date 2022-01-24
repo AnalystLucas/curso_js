305352488a04cf6444d097015e85bb3c421b8538
Lucas Moreira <lucasmoreiira.v@hotmail.com>
2022-01-24 20:16:55 -0300

    Função Arrow (Seta)

--- Secao_06/secao_06_parte4/app.js
+++ Secao_06/secao_06_parte4/app.js
@@ -3,11 +3,28 @@
 //     let mediaNum = (n1 + n2 + n3 ) / 3;
 //     return mediaNum;
 // }
+// console.log(media(9,7,8));
 
 //Expressão de Funções Anonima
-const media = function (n1, n2, n3){
-    let mediaNum = (n1 + n2 + n3 ) / 3;
-    return mediaNum;
-}
+// const media = function (n1, n2, n3){
+//     let mediaNum = (n1 + n2 + n3 ) / 3;
+//     return mediaNum;
+// }
+// console.log(media(9,7,8));
 
-console.log(media(9,7,8));
+//Arrow Function
+// const media = (n1, n2 ,n3) =>{
+//     let mediaNum = (n1 + n2 + n3) / 3;
+//     return mediaNum;
+// }
+// console.log(media(9,7,8));
+
+//Arrow Function sem chaves simplificando
+// const areaQuadrado = lado => {
+//     return lado * lado;
+// }
+// console.log(areaQuadrado(10));
+
+//Arrow Function mais simplificada dependendo do cenário sem retunr e chaves "{}"
+const areaQuadrado = lado => lado * lado;
+console.log(areaQuadrado(10));
