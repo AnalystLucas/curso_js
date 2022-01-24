b46c687107cddcf5b6a3bd43416bc0888d094e5f
Lucas Moreira <lucasmoreiira.v@hotmail.com>
2022-01-24 03:48:32 -0300

    Função Calcular Media

--- Secao_06/secao_06_parte2/app.js
+++ Secao_06/secao_06_parte2/app.js
@@ -1,18 +1,6 @@
-console.log("Meteorologia");
+function calculaMedia(num1, num2, num3){
+    let media = (num1 + num2 + num3);
+    return media;
+};
 
-let temp;
-let soma = 0;
-let media = 0;
-let cont = 0;
-
-while(true){
-    temp = parseInt(prompt("Digita o valor de Temperatura"));
-    soma += temp; //Somando as Temperaturas.
-
-    if(temp == 0){
-        media = soma; //Recebendo as Somatorias de Temperaturas
-        console.log(media / cont); // Calculando a Media e Exibindo.
-        break; //Saindo do Laço.
-    }
-    cont++;
-}
+console.log(calculaMedia(3,4,5));
--- Secao_06/secao_06_parte2/script.js
+++
@@ -1,4 +0,0 @@
-function calculaMedia(num1, num2, num3){
-    let media = (num1 + num2 + num3);
-    return media;
-};
