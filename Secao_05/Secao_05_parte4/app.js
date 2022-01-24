d50d3a8cf3bd0e60828853c48588fc53a376fb7d
Lucas Moreira <lucasmoreiira.v@hotmail.com>
2022-01-24 01:19:17 -0300

    Seção 05 parte 4 While

---
+++ Secao_05/Secao_05_parte4/app.js
@@ -0,0 +1,24 @@
+let valorEmprestimo;
+let idade = 24;
+let taxaDeJuros;
+let numAnos;
+let juros;
+let montante;
+
+if(idade >= 18 && idade <=25){
+    taxaDeJuros = 0.09;
+}else if(idade >= 26 && idade <=35){
+    taxaDeJuros = 0.08;
+}else if(idade >= 36 && idade <=50){
+    taxaDeJuros = 0.07;
+}else{
+    taxaDeJuros = 0.06;
+}
+
+for (let cont = 0; cont < 3; cont++){
+    valorEmprestimo = parseInt(prompt("Digite o valor do emprestimo"));
+    numAnos = parseInt(prompt("Digite o numero de anos"));
+    juros = valorEmprestimo * taxaDeJuros * numAnos;
+    montante = juros + valorEmprestimo;
+    console.log(montante);
+}
