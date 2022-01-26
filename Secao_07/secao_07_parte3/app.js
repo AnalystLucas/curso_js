00ca97bbfe9e7fbb492e05707c0987e2add02d05
Lucas Moreira <lucasmoreiira.v@hotmail.com>
2022-01-26 18:35:33 -0300

    Arrays e Metodos de Array interação com o mesmo

--- Secao_07/secao_07_parte3/app.js
+++ Secao_07/secao_07_parte3/app.js
@@ -7,8 +7,37 @@ frutas[1] = "Laranja";
 frutas[2] = "Maça"; 
 frutas[3] = "Limao";
 
-// console.log(frutas);
+console.log(frutas);
 
 frutas[2] = "Uva"; // Alterando o Indice 2 de Maça para Uva
-
 console.log(frutas);
+
+//Metodos dos Arrays.
+
+//Push
+frutas.push("Melancia"); //Adiciona Sempre ao Final do array
+console.log("Metodo push: " + frutas);
+
+//Unshift
+frutas.unshift("Caja"); //Adiciona sempre ao Inicio do Array.
+console.log("Metodo unshift: " + frutas);
+
+//Pop
+frutas.pop() //Remove o Sempre o Ultimo Elementos do Array.
+console.log("Metodo pop: " + frutas);
+
+//shift
+frutas.shift(); // Remove sempre o Primeiro Elementos do Array.
+console.log("Metodo shift: " + frutas);
+
+//Splice
+frutas.splice(3,1) // Removendo entre intervalos dentros dos indices do array sendo 3 o indice que vai iniciar e o 1 quantos elementos serão removidos apartir do indice 3
+console.log("Metodo splice: " + frutas);
+
+//sort
+frutas.sort();  //Ordenando em ondem crescente
+console.log("Metodo sort: " + frutas);
+
+//reverse
+frutas.reverse();  //Revertendo o array, caso for usado depois do sort fica em onde descrecente.
+console.log("Metodo reserver: " + frutas);
