95beb4d04c15a89f07181aa9df245f97790d3e3a
Lucas Moreira <lucasmoreiira.v@hotmail.com>
2022-01-26 17:44:41 -0300

    Desafio investigação Criminal com Funções

--- Secao_06/secao_06_parte6_desafio/app.js
+++ Secao_06/secao_06_parte6_desafio/app.js
@@ -1,33 +1,73 @@
-//Escopo de Função
+//Investigação criminal
 
-// let nome = "Lucas"; // Variavel Global !
+//Fazer 5 perguntas.
 
-// function sauda(){
-//     //Escopo da Função
-//     let saudacao = "Olá Sr(a). " + nome; // Variavel Local !
-//     return saudacao;
-// }
+//1 º Telefonou para a Vitima?
+//2 º Esteve no Local do Crime?
+//3 º Mora perto da Vitima?
+//4 º Devia para a Vitima?
+//5 º Ja trabalhou com a Vitima?
 
-// console.log(sauda());
+//Tabela de Classificação
+//5 resposta afirmativa = Assassino
+//3 a 4 resposta afirmativa = cumplice
+//2 resposta afirmativa = suspeito
+//0 a 1 resposta afirmativa = Inocente
 
-//Paramentros de uma função
+
+//Utilizando funções crie um sistema para realizar as perguntas e classificar conforme a tabela
 
 let nome;
-let snome;
-let numAnos;
-let ehBomPagador;
-let valorEmprestimo;
+let idade;
+let cont;
+let perguntas = [];
 
-function cadastrarCliente(nomecli, snomecli, valorEmprestimocli, numAnoscli = 2, ehBomPagadorcli = false ){
-    nome  = nomecli;
-    snome = snomecli;
-    numAnos = numAnoscli;
-    ehBomPagador = ehBomPagadorcli;
-    valorEmprestimo =  valorEmprestimocli;
+function suspeitoCrime(nomesus, idadesus){
+    nome = nomesus;
+    idade = idadesus;
+};
+function Questionario(){
+ perguntas = {
+        '1': "Telefonou para a Vitima?",
+        '2': "Esteve no Local do Crime?",
+        '3': "Mora perto da Vitima?",
+        '4': "Devia para a Vitima?",
+        '5': "Ja trabalhou com a Vitima?" 
+    }
+    return perguntas;   
+};
+function realizarQuestionario(){
+    let contper = 0;
+    perguntas = Questionario();
+    for(let i = 1; i <= 5; i++){
+        let confirmaper = confirm(perguntas[i]);
+        if (confirmaper == true){
+            contper++
+        }
+    }
+    cont = contper;
 }
 
-cadastrarCliente("Lucas", "Moreira", 15000);
-console.log(nome);
-console.log(snome);
-console.log(numAnos);
-console.log(ehBomPagador);
+function classificarQuestionario(){
+    if(cont == 5){
+        return "Assassino !!";
+    }else if(cont >=3 && cont <= 4){
+        return "Cumplice !";
+    }else if(cont == 2){
+        return "Suspeito";
+    }else if(cont >=0 && cont <= 1){
+        return "Inocente";
+    }else{
+        return "Não foi possivel classificar";
+    }
+}
+
+function Investigacao(){
+    
+    suspeitoCrime("Lucas", 26);
+    realizarQuestionario();
+    let classificar = classificarQuestionario();
+    console.log("Investigado: " + nome + " Idade: " + idade + " Você é: " + classificar)
+}
+
+Investigacao();
