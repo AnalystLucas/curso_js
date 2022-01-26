c791f58a148d3403b5f7708824a4acfbbb3bb940
Lucas Moreira <lucasmoreiira.v@hotmail.com>
2022-01-26 16:47:36 -0300

    Paramentros de Uma função

--- Secao_06/secao_06_parte5/app.js
+++ Secao_06/secao_06_parte5/app.js
@@ -1,11 +1,33 @@
 //Escopo de Função
 
-let nome = "Lucas"; // Variavel Global !
+// let nome = "Lucas"; // Variavel Global !
 
-function sauda(){
-    //Escopo da Função
-    let saudacao = "Olá Sr(a). " + nome; // Variavel Local !
-    return saudacao;
+// function sauda(){
+//     //Escopo da Função
+//     let saudacao = "Olá Sr(a). " + nome; // Variavel Local !
+//     return saudacao;
+// }
+
+// console.log(sauda());
+
+//Paramentros de uma função
+
+let nome;
+let snome;
+let numAnos;
+let ehBomPagador;
+let valorEmprestimo;
+
+function cadastrarCliente(nomecli, snomecli, valorEmprestimocli, numAnoscli = 2, ehBomPagadorcli = false ){
+    nome  = nomecli;
+    snome = snomecli;
+    numAnos = numAnoscli;
+    ehBomPagador = ehBomPagadorcli;
+    valorEmprestimo =  valorEmprestimocli;
 }
 
-console.log(sauda());
+cadastrarCliente("Lucas", "Moreira", 15000);
+console.log(nome);
+console.log(snome);
+console.log(numAnos);
+console.log(ehBomPagador);
